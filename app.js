89f0fc6b28812ee9577896fd462911c17af43ac3
Sazu01 <64630897+Sazu01@users.noreply.github.com>
2022-02-20 23:22:59 +0600

    Clean Code

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 "use strict";
 
+const heading = document.getElementsByTagName(`h1`);
+
 const input_user = document.querySelector(`input[type="text"]`);
 const input_pass = document.querySelector(`input[type="password"]`);
-const btn = document.querySelector(`input[type="submit"]`)
 
-// input_user.value
-
-
-const heading = document.getElementsByTagName("h1");
-
-console.dir(input_user);
-console.log(heading[0]);
+const btn = document.querySelector(`input[type="submit"]`);
 
 btn.addEventListener("click", (e) => {
     e.preventDefault()
-    if(input_user.value === "user" && input_pass.value === "1234"){
-        document.querySelector("div.thankyou").style.display = flex
+    if (input_user.value = "sazu" && input_pass.value = "1234"){
+        alert("Successful")
     }
 })
